35a522a0a8a446b812eac24331a4dd9f5881a357
Santiaguxo <41389812+Santiaguxo@users.noreply.github.com>
2022-06-01 21:29:37 -0400

    Update 1-Conceptos.py

--- Python/1-Conceptos.py
+++ Python/1-Conceptos.py
@@ -1,3 +1,13 @@
 print("Hello, World!")
 var = 1
 print(var)
+# create a function that count number between 1 and 100
+
+
+def count_number():
+    for i in range(1, 101):
+        print(i)
+
+
+# En Python la funcion aca cuando ya no se sigue el nivel de indentacion
+var = count_number()
